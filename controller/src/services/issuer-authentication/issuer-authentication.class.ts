fd57af9a21b8784e177856e8a4d2e3f3b5a13b0e
Emiliano Suñé <emiliano.sune@gmail.com>
2021-02-05 13:48:37 -0800

    Implemented issuer authentication endpoint to rotate api-key
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- controller/src/services/issuer-authentication/issuer-authentication.class.ts
+++ controller/src/services/issuer-authentication/issuer-authentication.class.ts
@@ -1,12 +1,16 @@
-import { NotImplemented } from '@feathersjs/errors';
-import { Params } from '@feathersjs/feathers';
+import { GeneralError, NotImplemented } from '@feathersjs/errors';
+import { Paginated, Params } from '@feathersjs/feathers';
 import {
   ServiceSwaggerAddon,
   ServiceSwaggerOptions,
 } from 'feathers-swagger/types';
 import { Application } from '../../declarations';
+import { IssuerProfile } from '../../models/issuer-model';
+import { v4 as uuidv4 } from 'uuid';
 
-interface Data {}
+interface Data {
+  'api-key': string;
+}
 
 interface ServiceOptions {}
 
@@ -21,7 +25,27 @@ export class IssuerAuthentication implements ServiceSwaggerAddon {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async create(data: Data, params?: Params): Promise<Data> {
-    return new NotImplemented();
+    const headers = params?.headers || {};
+    const apiKeyHeader = headers['issuer-api-key'] as string;
+
+    const findProfile = (await this.app.service('issuer-model').find({
+      query: { 'api-key': apiKeyHeader },
+      collation: { locale: 'en', strength: 1 },
+    })) as Paginated<IssuerProfile>;
+    const existingProfile = findProfile.data[0];
+
+    if (!existingProfile._id) {
+      throw new GeneralError(
+        'Unable to determine the identifier for the profile to be updated, please contact an administrator.'
+      );
+    }
+
+    const newApiKey = uuidv4();
+    await this.app.service('issuer-model').patch(existingProfile._id, {
+      'api-key': newApiKey,
+    });
+
+    return { 'api-key': newApiKey };
   }
 
   docs: ServiceSwaggerOptions = {
@@ -50,7 +74,7 @@ export class IssuerAuthentication implements ServiceSwaggerAddon {
     properties: {
       'api-key': {
         type: 'string',
-        description: 'The issuer\'s api-key',
+        description: "The issuer's api-key",
       },
     },
   };
